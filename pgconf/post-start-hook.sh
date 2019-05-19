f4038157e642b2fc230b2c52e3b62d80bc1b01df
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-05-19 21:40:37 +0200

    Make ready for subsequent DB starts

--- pgconf/post-start-hook.sh
+++ pgconf/post-start-hook.sh
@@ -7,27 +7,29 @@
 
 echo_info "Executing post-start-hook.." # add below this line
 
-echo "Waiting for PostgreSQL to start.."
-while true; do
-    pg_isready \
-    --host=${PG_PRIMARY_HOST} \
-    --port=${PG_PRIMARY_PORT} \
-    --username=${PG_PRIMARY_USER?} \
-    --timeout=2
-    if [ $? -eq 0 ]; then
-        echo "The database is ready."
-        break
-    fi
-    sleep 2
-done
+if [[ -v PGHOST ]]; then # Hack: Detects if DB starts for the first time (in this case PGHOST=/tmp; this is set in https://github.com/CrunchyData/crunchy-containers/blob/4dcbbf676523e613a571c3f79bb844d03643866f/bin/postgres/start.sh#L334)
+    echo "Waiting for PostgreSQL to start.."
+    while true; do
+        pg_isready \
+        --host=${PGHOST} \
+        --port=${PG_PRIMARY_PORT} \
+        --username=${PG_PRIMARY_USER?} \
+        --timeout=2
+        if [ $? -eq 0 ]; then
+            echo "The database is ready."
+            break
+        fi
+        sleep 2
+    done
 
-# Create additional DB users and grant privileges
-psql -d $PG_DATABASE --single-transaction \
--v PG_DATABASE=$PG_DATABASE \
--v PG_WRITE_USER=$PG_WRITE_USER \
--v PG_WRITE_PASSWORD=$PG_WRITE_PASSWORD \
--v PG_READ_USER=$PG_READ_USER \
--v PG_READ_PASSWORD=$PG_READ_PASSWORD \
--f /pgconf/grants.sql
+    # Create additional DB users and grant privileges
+    psql -d $PG_DATABASE --single-transaction \
+    -v PG_DATABASE=$PG_DATABASE \
+    -v PG_WRITE_USER=$PG_WRITE_USER \
+    -v PG_WRITE_PASSWORD=$PG_WRITE_PASSWORD \
+    -v PG_READ_USER=$PG_READ_USER \
+    -v PG_READ_PASSWORD=$PG_READ_PASSWORD \
+    -f /pgconf/grants.sql
+fi
 
 echo_info "Executing post-start-hook finished. Database is ready for use."
