39db2791ff36e31e011ba52c7e7279d7dbd227c8
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-07-19 10:41:28 +0200

    Switch back to AV CH model DM01AVCH24LV95D

--- tests/readTest.sql
+++ tests/readTest.sql
@@ -1,5 +1,5 @@
 -- Check for select privilege on tables and views
-SELECT * FROM stage.dm01avso24lv95fixpunktekatgrie1_lfp1;
-SELECT * FROM live.dm01avso24lv95fixpunktekatgrie1_lfp1;
+SELECT * FROM stage.dm01vch24lv95dfixpunktekatgrie1_lfp1;
+SELECT * FROM live.dm01vch24lv95dfixpunktekatgrie1_lfp1;
 SELECT * FROM stage.vw_oerebwms_weiteres_thema_einzelschutz_flaeche;
 SELECT * FROM live.vw_oerebwms_weiteres_thema_einzelschutz_flaeche;
