eb51d728174373718eca3afee22587b44462d344
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-07-29 10:32:33 +0200

    Re-generate SQL files after commit 91d2553

--- pgconf/setup.sql
+++ pgconf/setup.sql
@@ -7481,7 +7481,7 @@ CODE
   TID = ANY;
 END.
 
-','2019-07-25 16:08:44.501');
+','2019-07-29 10:25:09.416');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('PLZO-CH_LV95_1d_ili1.ili','1.0','PLZOCH1LV95D','
 TRANSFER INTERLIS1;
 
@@ -7704,7 +7704,7 @@ CODE
   TID = ANY;
 END.
 
-','2019-07-25 16:08:44.501');
+','2019-07-29 10:25:09.416');
 INSERT INTO live.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.createMetaInfo','True');
 INSERT INTO live.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.arrayTrafo','coalesce');
 INSERT INTO live.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.nameOptimization','disable');
@@ -15156,7 +15156,7 @@ CODE
   TID = ANY;
 END.
 
-','2019-07-25 16:08:40.637');
+','2019-07-29 10:25:05.962');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('PLZO-CH_LV95_1d_ili1.ili','1.0','PLZOCH1LV95D','
 TRANSFER INTERLIS1;
 
@@ -15379,7 +15379,7 @@ CODE
   TID = ANY;
 END.
 
-','2019-07-25 16:08:40.637');
+','2019-07-29 10:25:05.962');
 INSERT INTO stage.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.createMetaInfo','True');
 INSERT INTO stage.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.arrayTrafo','coalesce');
 INSERT INTO stage.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.nameOptimization','disable');
@@ -16966,7 +16966,7 @@ REFSYSTEM MODEL CoordSys (en) AT "http://www.interlis.ch/models"
 
 END CoordSys.
 
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREB_ExtractAnnex_V1_0.ili','2.3','OeREB_ExtractAnnex_V1_0{ CHAdminCodes_V1 LocalisationCH_V1 OeREBKRM_V1_1 CatalogueObjects_V1}','INTERLIS 2.3;
 
 /** Zusatzdaten für statischen OEREB-Auszug
@@ -17080,7 +17080,7 @@ VERSION "2019-07-24"  =
     
 
 END OeREB_ExtractAnnex_V1_0.
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREBKRMvs_V1_1.ili','2.3','OeREBKRMvs_V1_1{ CHAdminCodes_V1 LocalisationCH_V1 OeREBKRM_V1_1}','INTERLIS 2.3;
 
 /** Basisdefinition für Erlasse (Rechtsvorschriften, Hinweise auf Gesetzliche Grundlagen)
@@ -17202,7 +17202,7 @@ VERSION "2016-08-15"  =
   END HinweiseGesetzlicheGrundlagen;
 
 END OeREBKRMvs_V1_1.
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREBKRM_V1_1.ili','2.3','OeREBKRM_V1_1{ InternationalCodes_V1 LocalisationCH_V1 CatalogueObjects_V1}','INTERLIS 2.3;
 
 /** Basisdefinitionen für das OEREB-Katasterrahmenmodell
@@ -17366,7 +17366,7 @@ VERSION "2016-08-15"  =
   END CodelistenText;
 
 END OeREBKRM_V1_1.
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part1_GEOMETRY_20110830.ili','2.3','GeometryCHLV03_V1{ CoordSys Units INTERLIS} GeometryCHLV95_V1{ CoordSys Units INTERLIS}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -17544,7 +17544,7 @@ TYPE MODEL GeometryCHLV95_V1 (en)
 END GeometryCHLV95_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREBKRMtrsfr_V1_1.ili','2.3','OeREBKRMtrsfr_V1_1{ GeometryCHLV95_V1 CHAdminCodes_V1 LocalisationCH_V1 GeometryCHLV03_V1 OeREBKRM_V1_1 OeREBKRMvs_V1_1}','INTERLIS 2.3;
 
 /** Schnittstelle zwischen zuständiger Stelle für die Geobasisdaten und Katasterorganisation des Kantons.
@@ -17728,7 +17728,7 @@ VERSION "2016-08-15"  =
   END Transferstruktur;
 
 END OeREBKRMtrsfr_V1_1.
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part4_ADMINISTRATIVEUNITS_20110830.ili','2.3','CHAdminCodes_V1 AdministrativeUnits_V1{ CHAdminCodes_V1 InternationalCodes_V1 Dictionaries_V1 Localisation_V1 INTERLIS} AdministrativeUnitsCH_V1{ CHAdminCodes_V1 InternationalCodes_V1 LocalisationCH_V1 AdministrativeUnits_V1 INTERLIS}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -17948,7 +17948,7 @@ MODEL AdministrativeUnitsCH_V1 (en)
 END AdministrativeUnitsCH_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part2_LOCALISATION_20110830.ili','2.3','InternationalCodes_V1 Localisation_V1{ InternationalCodes_V1} LocalisationCH_V1{ InternationalCodes_V1 Localisation_V1} Dictionaries_V1{ InternationalCodes_V1} DictionariesCH_V1{ InternationalCodes_V1 Dictionaries_V1}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -18120,7 +18120,7 @@ MODEL DictionariesCH_V1 (en)
 END DictionariesCH_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('SO_AGI_AV_GB_Administrative_Einteilungen_Publikation_20180822.ili','2.3','SO_AGI_AV_GB_Administrative_Einteilungen_Publikation_20180822{ GeometryCHLV95_V1 CHAdminCodes_V1}','INTERLIS 2.3;
 
 /** !!------------------------------------------------------------------------------
@@ -18254,7 +18254,7 @@ VERSION "2018-08-22"  =
   END Nachfuehrungskreise;
 
 END SO_AGI_AV_GB_Administrative_Einteilungen_Publikation_20180822.
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part3_CATALOGUEOBJECTS_20110830.ili','2.3','CatalogueObjects_V1{ INTERLIS} CatalogueObjectTrees_V1{ INTERLIS CatalogueObjects_V1}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -18342,7 +18342,7 @@ MODEL CatalogueObjectTrees_V1 (en)
 END CatalogueObjectTrees_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 INSERT INTO live.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('Units-20120220.ili','2.3','Units','!! File Units.ili Release 2012-02-20
 
 INTERLIS 2.3;
@@ -18440,7 +18440,7 @@ CONTRACTED TYPE MODEL Units (en) AT "http://www.interlis.ch/models"
 
 END Units.
 
-','2019-07-25 16:08:45.141');
+','2019-07-29 10:25:10.063');
 -- INSERT INTO live.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.createMetaInfo','True');
 -- INSERT INTO live.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.arrayTrafo','coalesce');
 -- INSERT INTO live.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.nameOptimization','disable');
@@ -20047,7 +20047,7 @@ REFSYSTEM MODEL CoordSys (en) AT "http://www.interlis.ch/models"
 
 END CoordSys.
 
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREB_ExtractAnnex_V1_0.ili','2.3','OeREB_ExtractAnnex_V1_0{ CHAdminCodes_V1 LocalisationCH_V1 OeREBKRM_V1_1 CatalogueObjects_V1}','INTERLIS 2.3;
 
 /** Zusatzdaten für statischen OEREB-Auszug
@@ -20161,7 +20161,7 @@ VERSION "2019-07-24"  =
     
 
 END OeREB_ExtractAnnex_V1_0.
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREBKRMvs_V1_1.ili','2.3','OeREBKRMvs_V1_1{ CHAdminCodes_V1 LocalisationCH_V1 OeREBKRM_V1_1}','INTERLIS 2.3;
 
 /** Basisdefinition für Erlasse (Rechtsvorschriften, Hinweise auf Gesetzliche Grundlagen)
@@ -20283,7 +20283,7 @@ VERSION "2016-08-15"  =
   END HinweiseGesetzlicheGrundlagen;
 
 END OeREBKRMvs_V1_1.
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREBKRM_V1_1.ili','2.3','OeREBKRM_V1_1{ InternationalCodes_V1 LocalisationCH_V1 CatalogueObjects_V1}','INTERLIS 2.3;
 
 /** Basisdefinitionen für das OEREB-Katasterrahmenmodell
@@ -20447,7 +20447,7 @@ VERSION "2016-08-15"  =
   END CodelistenText;
 
 END OeREBKRM_V1_1.
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part1_GEOMETRY_20110830.ili','2.3','GeometryCHLV03_V1{ CoordSys Units INTERLIS} GeometryCHLV95_V1{ CoordSys Units INTERLIS}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -20625,7 +20625,7 @@ TYPE MODEL GeometryCHLV95_V1 (en)
 END GeometryCHLV95_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('OeREBKRMtrsfr_V1_1.ili','2.3','OeREBKRMtrsfr_V1_1{ GeometryCHLV95_V1 CHAdminCodes_V1 LocalisationCH_V1 GeometryCHLV03_V1 OeREBKRM_V1_1 OeREBKRMvs_V1_1}','INTERLIS 2.3;
 
 /** Schnittstelle zwischen zuständiger Stelle für die Geobasisdaten und Katasterorganisation des Kantons.
@@ -20809,7 +20809,7 @@ VERSION "2016-08-15"  =
   END Transferstruktur;
 
 END OeREBKRMtrsfr_V1_1.
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part4_ADMINISTRATIVEUNITS_20110830.ili','2.3','CHAdminCodes_V1 AdministrativeUnits_V1{ CHAdminCodes_V1 InternationalCodes_V1 Dictionaries_V1 Localisation_V1 INTERLIS} AdministrativeUnitsCH_V1{ CHAdminCodes_V1 InternationalCodes_V1 LocalisationCH_V1 AdministrativeUnits_V1 INTERLIS}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -21029,7 +21029,7 @@ MODEL AdministrativeUnitsCH_V1 (en)
 END AdministrativeUnitsCH_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part2_LOCALISATION_20110830.ili','2.3','InternationalCodes_V1 Localisation_V1{ InternationalCodes_V1} LocalisationCH_V1{ InternationalCodes_V1 Localisation_V1} Dictionaries_V1{ InternationalCodes_V1} DictionariesCH_V1{ InternationalCodes_V1 Dictionaries_V1}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -21201,7 +21201,7 @@ MODEL DictionariesCH_V1 (en)
 END DictionariesCH_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('SO_AGI_AV_GB_Administrative_Einteilungen_Publikation_20180822.ili','2.3','SO_AGI_AV_GB_Administrative_Einteilungen_Publikation_20180822{ GeometryCHLV95_V1 CHAdminCodes_V1}','INTERLIS 2.3;
 
 /** !!------------------------------------------------------------------------------
@@ -21335,7 +21335,7 @@ VERSION "2018-08-22"  =
   END Nachfuehrungskreise;
 
 END SO_AGI_AV_GB_Administrative_Einteilungen_Publikation_20180822.
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('CHBase_Part3_CATALOGUEOBJECTS_20110830.ili','2.3','CatalogueObjects_V1{ INTERLIS} CatalogueObjectTrees_V1{ INTERLIS CatalogueObjects_V1}','/* ########################################################################
    CHBASE - BASE MODULES OF THE SWISS FEDERATION FOR MINIMAL GEODATA MODELS
    ======
@@ -21423,7 +21423,7 @@ MODEL CatalogueObjectTrees_V1 (en)
 END CatalogueObjectTrees_V1.
 
 !! ########################################################################
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 INSERT INTO stage.T_ILI2DB_MODEL (filename,iliversion,modelName,content,importDate) VALUES ('Units-20120220.ili','2.3','Units','!! File Units.ili Release 2012-02-20
 
 INTERLIS 2.3;
@@ -21521,7 +21521,7 @@ CONTRACTED TYPE MODEL Units (en) AT "http://www.interlis.ch/models"
 
 END Units.
 
-','2019-07-25 16:08:43.885');
+','2019-07-29 10:25:08.729');
 -- INSERT INTO stage.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.createMetaInfo','True');
 -- INSERT INTO stage.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.arrayTrafo','coalesce');
 -- INSERT INTO stage.T_ILI2DB_SETTINGS (tag,setting) VALUES ('ch.ehi.ili2db.nameOptimization','disable');
@@ -21580,7 +21580,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.linie_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21608,7 +21608,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21636,7 +21636,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.punkt_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21664,7 +21664,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21692,7 +21692,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.linie_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21720,7 +21720,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21748,7 +21748,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.punkt_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21776,7 +21776,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21807,7 +21807,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.linie_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21835,7 +21835,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21863,7 +21863,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.punkt_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21891,7 +21891,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21919,7 +21919,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.linie_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21947,7 +21947,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -21975,7 +21975,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.punkt_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
@@ -22003,7 +22003,7 @@ AS
          geom.t_type,
          geom.t_ili_tid,
          geom.flaeche_lv95 AS geom,
-         eigbe.aussage,
+         eigbe.aussage_de AS aussage,
          eigbe.thema,
          eigbe.subthema AS sub_thema,
          eigbe.weiteresthema AS weiteres_thema,
