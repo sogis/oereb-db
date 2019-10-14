1e391621378126c22e3baa6012a4e588aab52cfa
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-10-14 17:40:49 +0200

    fix owner of additional mat views in sql/grants_gdi.sql too

--- sql/grants_gdi.sql
+++ sql/grants_gdi.sql
@@ -23,6 +23,9 @@ TO :PG_WRITE_USER;
 -- PG_WRITE_USER must be the owner of the materialized views
 -- so he can refresh them. See https://github.com/sogis/oereb-db/issues/12
 -- for a SQL command that may be used to generate these commands.
+ALTER TABLE live.vw_oerebwms_belastete_standorte_flaeche OWNER TO :PG_WRITE_USER;
+ALTER TABLE live.vw_oerebwms_grundwasserschutzareale_flaeche OWNER TO :PG_WRITE_USER;
+ALTER TABLE live.vw_oerebwms_grundwasserschutzzonen_flaeche OWNER TO :PG_WRITE_USER;
 ALTER TABLE live.vw_oerebwms_laermempfindlichkeitsstufen_flaeche OWNER TO :PG_WRITE_USER;
 ALTER TABLE live.vw_oerebwms_nutzungsplanung_baulinien_linie OWNER TO :PG_WRITE_USER;
 ALTER TABLE live.vw_oerebwms_nutzungsplanung_grundnutzung_flaeche OWNER TO :PG_WRITE_USER;
@@ -34,6 +37,9 @@ ALTER TABLE live.vw_oerebwms_waldabstandslinien_linie OWNER TO :PG_WRITE_USER;
 ALTER TABLE live.vw_oerebwms_waldgrenzen_linie OWNER TO :PG_WRITE_USER;
 ALTER TABLE live.vw_oerebwms_weiteres_thema_einzelschutz_flaeche OWNER TO :PG_WRITE_USER;
 ALTER TABLE live.vw_oerebwms_weiteres_thema_einzelschutz_punkt OWNER TO :PG_WRITE_USER;
+ALTER TABLE stage.vw_oerebwms_belastete_standorte_flaeche OWNER TO :PG_WRITE_USER;
+ALTER TABLE stage.vw_oerebwms_grundwasserschutzareale_flaeche OWNER TO :PG_WRITE_USER;
+ALTER TABLE stage.vw_oerebwms_grundwasserschutzzonen_flaeche OWNER TO :PG_WRITE_USER;
 ALTER TABLE stage.vw_oerebwms_laermempfindlichkeitsstufen_flaeche OWNER TO :PG_WRITE_USER;
 ALTER TABLE stage.vw_oerebwms_nutzungsplanung_baulinien_linie OWNER TO :PG_WRITE_USER;
 ALTER TABLE stage.vw_oerebwms_nutzungsplanung_grundnutzung_flaeche OWNER TO :PG_WRITE_USER;
